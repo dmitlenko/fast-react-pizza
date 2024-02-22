7d15bcdb5609bbed60526056b1c623b1dd95c143
Denys Mitlenko <softonka@gmail.com>
2024-02-22 22:55:10 +0200

    Add menu loader and useLoaderData hook in Menu component

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,19 @@
-function Menu() {
-  return <h1>Menu</h1>;
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant";
+import MenuItem from "./MenuItem";
+
+export async function loader() {
+  return await getMenu();
 }
 
-export default Menu;
+export default function Menu() {
+  const menu = useLoaderData();
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem key={pizza.id} pizza={pizza} />
+      ))}
+    </ul>
+  );
+}
