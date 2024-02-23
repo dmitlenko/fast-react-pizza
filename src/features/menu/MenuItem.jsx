09322559824e2791b33cc946cb2c60b7b8aee5da
Denys Mitlenko <softonka@gmail.com>
2024-02-23 14:17:27 +0200

    Add "Add to cart" functionality to MenuItem component

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,8 +1,24 @@
 import { formatCurrency } from "../../utils/helpers";
 import Button from "../../ui/Button";
 
+import { useDispatch } from "react-redux";
+import { addItem } from "../cart/cartSlice";
+
 function MenuItem({ pizza }) {
+  const dispatch = useDispatch();
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
+
+  const handleAddToCart = () => {
+    const newItem = {
+      pizzaId: id,
+      name,
+      unitPrice,
+      quantity: 1,
+      totalPrice: unitPrice,
+    };
+
+    dispatch(addItem(newItem));
+  };
 
   return (
     <li className="flex gap-4 py-2">
@@ -18,14 +34,17 @@ function MenuItem({ pizza }) {
         </p>
         <div className="item-center mt-auto flex justify-between">
           {!soldOut ? (
-            <p className="text-sm">{formatCurrency(unitPrice)}</p>
+            <>
+              <p className="text-sm">{formatCurrency(unitPrice)}</p>
+              <Button type="small" onClick={handleAddToCart}>
+                Add to cart
+              </Button>
+            </>
           ) : (
             <p className="text-sm font-medium uppercase text-stone-500">
               Sold out
             </p>
           )}
-
-          <Button type="small">Add to cart</Button>
         </div>
       </div>
     </li>
