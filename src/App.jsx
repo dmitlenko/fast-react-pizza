6a4bf016501beee0d604e09104b2ac68788acdec
Denys Mitlenko <softonka@gmail.com>
2024-02-23 16:41:51 +0200

    Add update order functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Order, { loader as orderLoader } from "./features/order/Order";
+import { action as updateOrderAction } from "./features/order/UpdateOrder";
 import Cart from "./features/cart/Cart";
 import CreateOrder, {
   action as createOrderAction,
@@ -34,6 +35,7 @@ const router = createBrowserRouter([
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        action: updateOrderAction,
       },
     ],
   },
